071c4e8fb6c47a3d6112d96786699ecd0c6ea699
Yulia M <amy.neal@rocketmail.com>
2019-03-05 15:30:01 +0000

    clean up code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const app = express();
@@ -17,28 +17,31 @@ const bcrypt = require('bcrypt');
 const bcryptSaltRounds = 12;
 const moment = require('moment');
 var user;
-//var userList = [];
+
 var userList = {};
 
 mongoose.connect(connectInfo, {
-    useNewUrlParser: true
-},  function(error){
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Connected to the databbase");
-}
+  useNewUrlParser: true
+}, function(error) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Connected to the databbase");
+  }
 });
 
-var chatSchema = new mongoose.Schema ({
-    username: String,
-    msg: String,
-    created: {type: Date, default: Date.now()}
+var chatSchema = new mongoose.Schema({
+  username: String,
+  msg: String,
+  created: {
+    type: Date,
+    default: Date.now()
+  }
 });
 var userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
+  name: String,
+  email: String,
+  password: String
 });
 
 var User = mongoose.model("User", userSchema); // create a model to use to have a template for each of the documents in the collection
@@ -50,231 +53,259 @@ app.set('trust proxy', 1); // trust first proxy, may need to remove this for her
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-    extended: true //default already, but must specify. Value can be any type when true
+  extended: true //default already, but must specify. Value can be any type when true
 }));
 
 app.use(session({
-    name: session_name,
-    resave: false,
-    saveUninitialized: false,
-    secret: session_secret,
-    trustProxy: true,
-    cookie: {
-        maxAge: sessionMaxAge,
-        sameSite: true,
-        secure: true
-    }
+  name: session_name,
+  resave: false,
+  saveUninitialized: false,
+  secret: session_secret,
+  trustProxy: true,
+  cookie: {
+    maxAge: sessionMaxAge,
+    sameSite: true,
+    secure: true
+  }
 }));
 
 const redirectLogin = (req, res, next) => {
-    if(!req.session.userId){
-        res.redirect('/login');
-    }else{
-        user = req.session.name;
-        next();
-    }
+  if (!req.session.userId) {
+    res.redirect('/login');
+  } else {
+    user = req.session.name;
+    next();
+  }
 };
 
 const redirectHome = (req, res, next) => {
-    if(req.session.userId){
-        user = req.session.name;
-        res.redirect('/home');
-    }else{
-        next();
-    }
+  if (req.session.userId) {
+    user = req.session.name;
+    res.redirect('/home');
+  } else {
+    next();
+  }
 };
 
-app.get('/', (req, res) => {
-    const { userId } = req.session;
-    //const userId = 1;
-    // ${} means insert this into string
-    res.send(`
-    <h1>Welcome!</h1>
-    ${userId ? `
-    
-    <a href="/">Home</a>
-    <form method="post" action="/logout">
-        <button>Logout</button>
-    </form>
-    ` : `
-    <a href="/login">Login</a>
-    <a href="/register">Register</a>
-    `}
-    `);
+app.get('/', redirectHome, (req, res) => {
+  res.render('login');
 });
 
 app.get('/home', redirectLogin, (req, res) => {
-     res.render('home', { name: req.session.name}); 
+  res.render('home', {
+    name: req.session.name
+  });
 });
 
 app.get('/login', redirectHome, (req, res) => { // have a login html file for this
-    res.render('login');
+  res.render('login', {
+    error: ''
+    
+  });
 });
 
 app.get('/register', redirectHome, (req, res) => {
-    res.render('register', { error: req.session.error});
-    
+  
+  res.render('register', {
+    error: ''
+  });
+
 });
 
 app.post('/login', redirectHome, (req, res) => {
-    const { email, password } = req.body;
-    if(email && password){
-        User.find({email: email}, function(error, data){
-            if(error){
-                console.log(error);
-            }if(data[0] !== undefined){
-            // decrypt and confirm password
-            
-            var match = bcrypt.compareSync(password ,data[0].password);
-            
-                if(match === true){
-                    req.session.userId = data[0]._id;
-                    req.session.name = data[0].name;
-                
-                return res.redirect('/home');
-                }else{
-                    
-                    res.redirect('/login');     
-                }
-            }else{
-                
-                res.redirect('/login'); 
-            }
-                
-            });
-  }});
+  const {
+    email,
+    password
+  } = req.body;
+ 
+  if (email && password) {
+    User.find({
+      email: email
+    }, function(error, data) {
+      if (error) {
+        console.log(error);
+      }
+      if (data[0] !== undefined) { // found email in database
+        // decrypt and confirm password
+
+        var match = bcrypt.compareSync(password, data[0].password);
+
+        if (match === true) {
+          req.session.userId = data[0]._id;
+          req.session.name = data[0].name;
+
+          return res.redirect('/home');
+        } else if (match != true) {
+          console.log("Incorrect username or password");
+          res.render('login', {
+        error: "Incorrect username or password"
+      });
+         
+        }
+      } else {
+
+        res.redirect('/login');
+      }
+
+    });
+  }
+});
 
 app.post('/register', redirectHome, (req, res) => {
-    const { name, email, password } = req.body;
-    User.find({email: email}, function(error, data){
-       
-        if(error){
-           console.log(error);
-           return;
-        }
-        if(data.length > 0){ // email already exists in database
-           req.session.error = "Email already registered";
-           res.render('register', { error: "Email already registered"}); 
-        }
-        else if(data.length == 0){ // if email not found
-            bcrypt.hash(password, bcryptSaltRounds)
-            .then(function(hashedPassword){
-                User.create({
-                name: name,
-                email: email,
-                password: hashedPassword
+  const {
+    name,
+    email,
+    password
+  } = req.body;
+  User.find({
+    email: email
+  }, function(error, data) {
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+    if (data.length > 0) { // email already exists in database
+      req.session.registerError = "Email already registered";
+      res.render('register', {
+        error: "Email already registered"
+      });
+    } else if (data.length == 0) { // if email not found
+      bcrypt.hash(password, bcryptSaltRounds)
+        .then(function(hashedPassword) {
+          User.create({
+              name: name,
+              email: email,
+              password: hashedPassword
             })
-            .then(function(){
-                User.find({email: email}, function(error, data){
-           if(error){
-               console.log(error);
-           }
-            req.session.userId = data[0]._id;
-            req.session.name = data[0].name;
-            return res.redirect('/home');
-        })
-        .catch(function(error){
-            console.log("Error with registration");
-            console.log(error);
-            
+            .then(function() {
+              User.find({
+                  email: email
+                }, function(error, data) {
+                  if (error) {
+                    console.log(error);
+                  }
+                  req.session.userId = data[0]._id;
+                  req.session.name = data[0].name;
+                  return res.redirect('/home');
+                })
+                .catch(function(error) {
+                  console.log("Error with registration");
+                  console.log(error);
+
+                });
+            });
         });
-            });
-            });
-            // hash the passwords for real site
-            
-            
-        }else{
-            return res.redirect('/register');
-        }
-        
-        }
-)});
+     
+    } else {
+      return res.redirect('/register');
+    }
+
+  })
+});
 
 app.get('/logout', redirectLogin, (req, res) => { //fix this
-    req.session.destroy(err => {
-        if (err) {
-            return res.redirect('/home');
-            
-        }
-        res.clearCookie(session_name);
-        res.redirect('/login');
-    });
-});
+  req.session.destroy(err => {
+    if (err) {
+      return res.redirect('/home');
 
-io.on('connection', function(socket){
-    
-    var query = Chat.find({});
-    query.sort('-created').limit(10).exec(function(err, docs){
-        if(err){
-            throw err;
-        }
-      
-        socket.emit('load old msg', docs);
-    });
-    
-    function updateUserList(){
-        io.emit('usernames', Object.keys(userList)); // returns an array of a given object's own property names, in the same order as we get with a normal loop
     }
- 
-  socket.on('chat message', function(data, callback){
-     var msg = data.trim();
-     if(msg.substr(0,3) === '/p '){
-         console.log("private message");
-         var msg = msg.substr(3);
-         var ind = msg.indexOf(' ');
-         if(ind !== -1){
-             var name = msg.substring(0, ind);
-             var msg = msg.substring(ind + 1);
-             if(name in userList){
-                // display message to sender and private recipient only
-                 userList[name].emit('private', {msg: msg, username: socket.username} );
-                 socket.emit('private', {msg: msg, username: socket.username} );
-                  
-             }else{
-                 callback('Error! User not online.');
-             }
-            
-         }else{
-             callback('Error! Please follow the format for sending a private message.');
-         }
-     }else{
-        var newMsg = new Chat({msg: msg, username: socket.username});
-        console.log("chat msg from " + socket.username );
-       
-        newMsg.save(function(err){
-            if(err){
-                throw err;
-            }
-            io.emit('chat message', {msg: data, username: socket.username, created: newMsg.created} ); 
-        });
-        
-     }
+    res.clearCookie(session_name);
+    res.redirect('/login');
   });
- 
-  socket.on('spam message', function(msg){
-     console.log(msg);
-     socket.emit('spam message', 'Spam detected, incorrect format.');
+});
+
+io.on('connection', function(socket) {
+
+  var query = Chat.find({});
+  query.sort('-_id').limit(20).exec(function(err, docs) {
+    if (err) {
+      throw err;
+    }
+
+    socket.emit('load old msg', docs);
   });
-  
-  socket.on('new user', function(data){
-     if(!(data in userList)){
-         console.log("new user, not in userList");
-         socket.username = data;
-         userList[socket.username] = socket;
-         updateUserList();
-     } 
-  });
-  
-  socket.on('disconnect', function(data){
-      if(!socket.username){
-        return;  
+
+  function updateUserList() {
+    io.emit('usernames', Object.keys(userList)); // returns an array of a given object's own property names, in the same order as we get with a normal loop
+  }
+
+  socket.on('chat message', function(data, callback) {
+    var msg = data.trim();
+    if (msg.substr(0, 3) === '/p ') {
+      console.log("private message");
+      var msg = msg.substr(3);
+      var ind = msg.indexOf(' ');
+      if (ind !== -1) {
+        var name = msg.substring(0, ind);
+        var msg = msg.substring(ind + 1);
+        if (name in userList) {
+          // display message to sender and private recipient only
+          userList[name].emit('private', {
+            msg: msg,
+            username: socket.username
+          });
+          socket.emit('private', {
+            msg: msg,
+            username: socket.username
+          });
+
+        } else {
+          callback('Error! User not online.');
+        }
+
+      } else {
+        callback('Error! Please follow the format for sending a private message.');
       }
-      delete userList[socket.username];
+    } else {
+      var newMsg = new Chat({
+        msg: msg,
+        username: socket.username
+      });
+      console.log("chat msg from " + socket.username);
+
+      newMsg.save(function(err) {
+        if (err) {
+          throw err;
+        }
+        io.emit('chat message', {
+          msg: data,
+          username: socket.username,
+          created: newMsg.created
+        });
+      });
+
+    }
+  });
+
+  socket.on('spam message', function(msg) {
+    console.log(msg);
+    socket.emit('spam message', 'Spam detected, incorrect format.');
+  });
+
+  socket.on('new user', function(data) {
+    if (!(data in userList)) {
+      console.log("new user, not in userList: " + data);
+      socket.username = data;
+      userList[socket.username] = socket;
       updateUserList();
+    }
+    if(data in userList){
+      socket.username = data;
+      updateUserList();
+    }
+  });
+
+  socket.on('disconnect', function(data) {
+    if (!socket.username) {
+      return;
+    }
+    delete userList[socket.username];
+    updateUserList();
   });
 
 });
- 
-http.listen(port, function(){
-    console.log("listening on specified port");
+
+http.listen(port, function() {
+  console.log("listening on specified port");
 })
--- views/message_handle.js
+++ views/message_handle.js
@@ -1,118 +1,118 @@
- ("message_handle.js accessed");
-var form = document.getElementById('message-form');
-var messages = document.getElementById('messages');
-var chat_area = document.getElementById('chat-area');
+
+ var form = document.getElementById('message-form');
+ var messages = document.getElementById('messages');
+ var chat_area = document.getElementById('chat-area');
 
 
-var socket = io();
-var name = document.getElementById('name').innerText;
-var user = name.slice(6);
-var whosOnlineList = document.getElementById('whosOnlineList');
-var $whosOnlineList = $("#whosOnlineList");
+ var socket = io();
+ var name = document.getElementById('name').innerText;
+ var user = name.slice(6);
+ var whosOnlineList = document.getElementById('whosOnlineList');
+ var $whosOnlineList = $("#whosOnlineList");
 
-function displayModal(e){
-var $private_modal = $("#private_modal");
-var $private_modal_content = $("#private_modal_content");
-var $close = $("#close");
- ("displayModul");
-}
+ function displayModal(e) {
+   var $private_modal = $("#private_modal");
+   var $private_modal_content = $("#private_modal_content");
+   var $close = $("#close");
+   ("displayModul");
+ }
 
-function bottomScroll(){
-    chat_area.scrollTop = chat_area.scrollHeight;
-    return;
-}
+ function bottomScroll() {
+   chat_area.scrollTop = chat_area.scrollHeight;
+   return;
+ }
 
-window.onload = function(){
-     ("home page loaded");
-    socket.emit('new user', user);
-};
+ window.onload = function() {
+   ("home page loaded");
+   socket.emit('new user', user);
+ };
 
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    var typeMessage = document.getElementById('type-message').value;
-    
-    var regex = new RegExp(/[\s]/);
-    if (typeMessage != '' && !(typeMessage.length > 50 && regex.test(typeMessage) != true)){
-        var msg = typeMessage;
-        socket.emit("chat message", msg, function(data){
-            var newMsg = document.createElement('div');
-            newMsg.setAttribute('class', 'error');
-            messages.appendChild(newMsg).innerHTML = data;
-            
-        });
-        form.reset();    
-    }else{
-         ("spam");
-        socket.emit('spam message', 'Spam detected, message not sent.');
-    }
-    
-});
-    
-socket.on('chat message', function(data){
+ form.addEventListener('submit', function(e) {
+   e.preventDefault();
+   var typeMessage = document.getElementById('type-message').value;
+
+   var regex = new RegExp(/[\s]/);
+   if (typeMessage != '' && !(typeMessage.length > 50 && regex.test(typeMessage) != true)) {
+     var msg = typeMessage;
+     socket.emit("chat message", msg, function(data) {
+       var newMsg = document.createElement('div');
+       newMsg.setAttribute('class', 'error');
+       messages.appendChild(newMsg).innerHTML = data;
+
+     });
+     form.reset();
+   } else {
+     ("spam");
+     socket.emit('spam message', 'Spam detected, message not sent.');
+   }
+
+ });
+
+ socket.on('chat message', function(data) {
    displayMsg(data);
-      
-});
-socket.on('spam message', function(msg){
+
+ });
+ socket.on('spam message', function(msg) {
    // handle spam here 
    var newMsg = document.createElement('div');
-    ("spam detected, message does not meet correct format");
+   ("spam detected, message does not meet correct format");
    messages.appendChild(newMsg).innerText = msg;
-});
+ });
 
-socket.on('usernames', function(data){
-    ("list of usernames: " + data);
-   var html='';
-   for(var i=0;i<data.length;i++){
-       html += '<li class="whos-online-user" id=' + data[i] + '>' + data[i] + '<br/>'; //overwrites any existing contents
+ socket.on('usernames', function(data) {
+   ("list of usernames: " + data);
+   var html = '';
+   for (var i = 0; i < data.length; i++) {
+     html += '<li class="whos-online-user" id=' + data[i] + '>' + data[i] + '<br/>'; //overwrites any existing contents
    }
    $whosOnlineList.html(html);
-});
+ });
 
-socket.on('private', function(data){
-    var date = new Date();
-    var dateTime = date.toLocaleDateString() + " " + date.toLocaleTimeString() + "  (private)";
-    var newMsg = document.createElement('div');
-    newMsg.setAttribute('class', 'private');
-    var when = document.createElement('span');
-    when.setAttribute("class", "date");
-    var userName = document.createElement('span');
-    var userMsg = document.createElement('span');
-    var spaceBetween = document.createElement('span');
-    messages.appendChild(newMsg);
-    newMsg.appendChild(when).innerHTML = dateTime + "<br>";
-    newMsg.appendChild(userName).innerHTML= data.username;
-    newMsg.appendChild(spaceBetween).innerHTML= "   :   " ;
-    newMsg.appendChild(userMsg).innerHTML=data.msg;
-    bottomScroll();  
-});
+ socket.on('private', function(data) {
+   var date = new Date();
+   var dateTime = date.toLocaleDateString() + " " + date.toLocaleTimeString() + "  (private)";
+   var newMsg = document.createElement('div');
+   newMsg.setAttribute('class', 'private');
+   var when = document.createElement('span');
+   when.setAttribute("class", "date");
+   var userName = document.createElement('span');
+   var userMsg = document.createElement('span');
+   var spaceBetween = document.createElement('span');
+   messages.appendChild(newMsg);
+   newMsg.appendChild(when).innerHTML = dateTime + "<br>";
+   newMsg.appendChild(userName).innerHTML = data.username;
+   newMsg.appendChild(spaceBetween).innerHTML = "   :   ";
+   newMsg.appendChild(userMsg).innerHTML = data.msg;
+   bottomScroll();
+ });
 
-socket.on('load old msg', function(docs){
-     ("displaying old messages!");
-    //for(var i=docs.length-1;i>=0; i--){
-      for(var i=0; i<docs.length;i++){
-        displayMsg(docs[i]);
-    }
-   
-});
+ socket.on('load old msg', function(docs) {
+   ("displaying old messages!");
+   for (var i = docs.length - 1; i >= 0; i--) {
+     //for(var i=0; i<docs.length;i++){
+     displayMsg(docs[i]);
+   }
 
-function displayMsg(data){
-    var date = new Date(data.created);
-    var dateTime = date.toLocaleDateString() + " " + date.toLocaleTimeString();
-   
-    var newMsg = document.createElement('div');
-    var when = document.createElement('span');
-    //var brk = document.createElement('br');
-    when.setAttribute("class", "date");
-    newMsg.setAttribute("class", "newMsg");
-    var userName = document.createElement('span');
-    userName.setAttribute("id", "msgUsername");
-    var userMsg = document.createElement('span');
-    userMsg.setAttribute("class", "user-msg");
-    var spaceBetween = document.createElement('span');
-    messages.appendChild(newMsg);
-    newMsg.appendChild(when).innerHTML = dateTime + "<br>";
-    newMsg.appendChild(userName).innerHTML= data.username;
-    newMsg.appendChild(spaceBetween).innerHTML= "   :   " ;
-    newMsg.appendChild(userMsg).innerHTML=data.msg;
-    bottomScroll();  
-}
+ });
+
+ function displayMsg(data) {
+   var date = new Date(data.created);
+   var dateTime = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+
+   var newMsg = document.createElement('div');
+   var when = document.createElement('span');
+   //var brk = document.createElement('br');
+   when.setAttribute("class", "date");
+   newMsg.setAttribute("class", "newMsg");
+   var userName = document.createElement('span');
+   userName.setAttribute("id", "msgUsername");
+   var userMsg = document.createElement('span');
+   userMsg.setAttribute("class", "user-msg");
+   var spaceBetween = document.createElement('span');
+   messages.appendChild(newMsg);
+   newMsg.appendChild(when).innerHTML = dateTime + "<br>";
+   newMsg.appendChild(userName).innerHTML = data.username;
+   newMsg.appendChild(spaceBetween).innerHTML = "   :   ";
+   newMsg.appendChild(userMsg).innerHTML = data.msg;
+   bottomScroll();
+ }
